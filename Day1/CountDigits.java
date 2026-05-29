86fb26b6b82a9f73dcd7972d830b0d75e5579473
Rishabh1104-k <rishabhkishor1104@gmail.com>
2026-05-29 12:09:28 +0530

    Day 2 assignment

--- Day1/CountDigits.java
+++ Day1/CountDigits.java
@@ -1,17 +1,17 @@
 import java.util.Scanner;
 
 public class CountDigits {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter a number: ");
-        int num = sc.nextInt();
+System.out.print("Enter a number: ");
+int num = sc.nextInt();
 
-        int count = 0;
+int count = 0;
 
-        while (num != 0) {
-            num = num / 10;
-            count++;
+while (num != 0) {
+num = num / 10;
+count++;
         }
 
         System.out.println("Number of digits = " + count);
